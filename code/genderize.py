cac4a8e3cfe29c7ce4ebbd31baec157a99b35ca0
vnling <veenling@gmail.com>
2022-11-27 16:43:55 +0400

    add gender data from genderize queries

---
+++ code/genderize.py
@@ -0,0 +1,25 @@
+"""
+takes a comma-separated data file name as command line arg
+the first element in each line in the file should be a name
+generates a result file with the original name and the genderize result for the first name
+"""
+
+import sys
+import requests
+
+data_file = sys.argv[1]
+result_file = 'all_current_genderize_result.csv'
+
+with open(data_file, 'r', encoding='utf-8') as f:
+    with open(result_file, 'a', encoding='utf-8') as g:
+        for line in f:
+            full_name = line.split(',')[0]
+            first_name = full_name.split(' ')[0]
+            url = 'https://api.genderize.io?name=' + first_name 
+            r = requests.get(url)
+            # protect against null gender values (name not in genderize db)
+            if r.json()['count']:
+                result_line = line.rstrip('\n') + ',' + r.json()['gender'] + ',' + str(r.json()['probability']) + '\n'
+            else:
+                result_line = line.rstrip('\n') + ',' + 'None' + ',' + str(r.json()['probability']) + '\n'
+            g.write(result_line)
