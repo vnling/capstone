5e45a6e8d9ac26f9f59d116709f464117bb66400
Vee Nis Ling <veenling@gmail.com>
2023-05-03 13:08:50 +0400

    redo citations matching

--- code/clean_citations.py
+++ code/clean_citations.py
@@ -1,23 +1,36 @@
 import pandas as pd
 
 journal = "aer"
-authors = pd.read_csv(f"./{journal}_author_citations.csv", 
+
+authors = pd.read_csv(f"./{journal}_authors_cleaned.csv", usecols=['Year', 'AuthorId']).dropna(subset=['AuthorId'])
+authors = authors.astype({'AuthorId': int})
+authors = authors.rename(columns={'Year': 'PaperYear'})
+authors = authors.sort_values(by=['PaperYear'])
+authors = authors.drop_duplicates(subset=['AuthorId'], keep='last')
+
+editors = pd.read_csv(f"{journal}_random_duplicates.csv", usecols=['Name', 'AuthorId']).dropna(subset=['AuthorId'])
+editors = editors.astype({'AuthorId': int})
+tenures = pd.read_csv(f"{journal}.tenures.csv", usecols=['Name', 'End'])
+editors = editors.merge(tenures, on=['Name'], how='inner')
+editors = editors.rename(columns={'End': 'PaperYear'})
+editors = editors.sort_values(by=['PaperYear'])
+editors = editors.drop_duplicates(subset=['AuthorId'], keep='last')
+
+author_citations = pd.read_csv(f"./{journal}_author_citations.csv", 
                       usecols=['AuthorId','PaperId','Year_paper','OriginalAuthor_left','CitesFrom','Year_citation'])
-editors = pd.read_csv(f"./{journal}_editor_citations.csv", 
+editor_citations = pd.read_csv(f"./{journal}_editor_citations.csv", 
                       usecols=['AuthorId','PaperId','Year_paper','Name','CitesFrom','Year_citation'])
 
-authors = authors.sort_values(by=['Year_paper'])
-editors = editors.sort_values(by=['Year_paper'])
-authors = authors[authors['Year_paper'] >= authors['Year_citation']]
-editors = editors[editors['Year_paper'] >= editors['Year_citation']]
+authors = authors.join(author_citations.set_index('AuthorId'), on=['AuthorId'], how='left')
+editors = editors.join(editor_citations.set_index('AuthorId'), on=['AuthorId'], how='left', lsuffix="_left", rsuffix="_right")
+
+authors = authors[authors['PaperYear'] >= authors['Year_citation']]
+editors = editors[editors['PaperYear'] >= editors['Year_citation']]
 authors['PaperCount'] = authors['AuthorId'].groupby(authors.AuthorId).transform('count')
 editors['PaperCount'] = editors['AuthorId'].groupby(editors.AuthorId).transform('count')
-
-authors.head(20)
-editors.head(20)
 
 authors = authors.drop_duplicates(subset=['AuthorId'], keep='last')
 editors = editors.drop_duplicates(subset=['AuthorId'], keep='last')
 
-authors.to_csv('{journal}_authors_citations_final.csv')
-editors.to_csv('{journal}_editors_citations_final.csv')
+authors.to_csv(f'{journal}_authors_citations_final.csv')
+editors.to_csv(f'{journal}_editors_citations_final.csv')
